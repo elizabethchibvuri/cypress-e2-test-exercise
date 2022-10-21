6e3d613ccd093ab7cfe1d0d931884f30b1713375
Michael Neal <michaelneal9675@gmail.com>
2022-10-21 15:08:45 +0200

    tried some typing tests

--- roadmap/cypress/e2e/doTheThing.cy.ts
+++ roadmap/cypress/e2e/doTheThing.cy.ts
@@ -1,9 +1,51 @@
 describe('empty spec', () => {
-  beforeEach(() => {
+  let emailInput: Cypress.Chainable<JQuery<HTMLElement>>
+  let submitEmailButton: Cypress.Chainable<JQuery<HTMLElement>>
+
+  function getComponents() {
+    emailInput = cy.get('#emailInput')
+    submitEmailButton = cy.get('#submitEmailButton')
+  }
+
+  function initializeComponents() {
+    emailInput.clear()
+  }
+
+  before(() => {
     cy.visit('http://localhost:3000')
   })
 
-  it('passes', () => {
-    cy.get('#emailInput').type('blah').should('have.value', 'blah')
+  beforeEach(() => {
+    getComponents()
+    initializeComponents()
+  })
+
+  it('can type in the input', () => {
+    emailInput
+      .type('blah')
+      .should('have.value', 'blah')
+      .type('aoeu')
+      .should('have.value', 'blahaoeu')
+  })
+
+  it('can type in the input as well', () => {
+    emailInput
+      .type('blah')
+      .should('have.value', 'blah')
+      .clear()
+      .type('aoeu')
+      .should('have.value', 'aoeu')
+  })
+
+  it('can click the email submission button', () => {
+    emailInput.should('have.value', '')
+    submitEmailButton.click()
+    emailInput.should('have.value', '')
+  })
+
+  it('clicking email submit button clears the email input field', () => {
+    emailInput.type('blah').should('have.value', 'blah')
+    submitEmailButton.click()
+    emailInput.should('have.value', '')
   })
 })
--- roadmap/pages/index.tsx
+++ roadmap/pages/index.tsx
@@ -247,6 +247,7 @@ export default function Roadmap({
                 className="px-3 py-3 mt-1 text-lg block w-full border border-gray-200 rounded-md text-gray-900 placeholder-gray-400 focus:outline-none focus:ring focus:ring-blue-300"
               />
               <button
+                id="submitEmailButton"
                 className="flex items-center justify-center absolute right-2 top-2 px-4 h-10 border border-gray-200 text-gray-900 rounded-md w-14 focus:outline-none focus:ring focus:ring-blue-300 focus:bg-gray-100"
                 type="submit">
                 {isEmailLoading ? <LoadingSpinner /> : 'OK'}
